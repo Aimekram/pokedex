be2da4af6f585c2160f7d61eeabbcbf9d67f1465
Aimekram <janina.byszewska@gmail.com>
2020-04-15 21:19:19 +0200

    Flip card on click

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Pokemon = ({ name, moreData, front }) => {
-  const { sprites, weight } = moreData;
-  return front ? (
-    <div className="pokemoncard">
-      <img className="pokemoncard_img" src={sprites.front_default} alt={name} />
-      <p className="pokemoncard_name">{name}</p>
-    </div>
-  ) : (
-    <div className="pokemoncard">
-      <p>Weight: {weight}</p>
-    </div>
-  );
-};
+class Pokemon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      front: true,
+    };
+    this.flip = this.flip.bind(this);
+  }
+
+  flip() {
+    this.setState((prevState) => ({ front: !prevState.front }));
+  }
+
+  render() {
+    const { front } = this.state;
+    const { name, moreData } = this.props;
+    const { sprites, weight } = moreData;
+
+    return (
+      <div className="pokemoncard" onClick={this.flip}>
+        {front ? (
+          <>
+            <img
+              className="pokemoncard_img"
+              src={sprites.front_default}
+              alt={name}
+            />
+            <p className="pokemoncard_name">{name}</p>
+          </>
+        ) : (
+          <>
+            <p>Weight: {weight} hectograms</p>
+          </>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Pokemon;
