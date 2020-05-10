cf5bfc48f098a74691c11625468e7469363c368b
Aimekram <janina.byszewska@gmail.com>
2020-05-10 12:29:51 +0200

    Updates

---
+++ precache-manifest.327e88c808014b8bbc5863389db13806.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6547cbe43f95a5738d39d0f74afe9cbd",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "a5320ce6c526eb7c6e21",
+    "url": "/pokedex/static/css/main.41b61e61.chunk.css"
+  },
+  {
+    "revision": "51c54a9fb36d986eeaa6",
+    "url": "/pokedex/static/js/2.829709f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.829709f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5320ce6c526eb7c6e21",
+    "url": "/pokedex/static/js/main.b9e83f89.chunk.js"
+  },
+  {
+    "revision": "601f8d113175821311bd",
+    "url": "/pokedex/static/js/runtime-main.0ec6b405.js"
+  },
+  {
+    "revision": "900f8add35323c44ee840c611ed9a7a5",
+    "url": "/pokedex/static/media/PokemonClassic.900f8add.ttf"
+  },
+  {
+    "revision": "6e5393a64ca23c455e8568d409e82da6",
+    "url": "/pokedex/static/media/PokemonHollow.6e5393a6.ttf"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/pokedex/static/media/PokemonSolid.36d26dbc.ttf"
+  },
+  {
+    "revision": "78b5090a79e5bae4654abba2edb7f00b",
+    "url": "/pokedex/static/media/background.78b5090a.jpg"
+  },
+  {
+    "revision": "47c9fa6d91efcfcdc06fbe0a2f0c22e2",
+    "url": "/pokedex/static/media/card_background.47c9fa6d.png"
+  }
+]);
